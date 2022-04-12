17c1874013c06f82527e58186a6ce7722a811454
Brian Grossman <bgrossman@alum.mit.edu>
2022-04-12 10:44:27 -0400

    Added dragging

--- Imirsiv/scripts/main.js
+++ Imirsiv/scripts/main.js
@@ -31,14 +31,67 @@ document.addEventListener('mouseup', e => {
                 newNotecard = document.createElement("div");
 
                 // give it a class notecard
-                newNotecard.classList.add("notecard")
+                newNotecard.classList.add("notecard");
+
+                // add draggable class
+                newNotecard.classList.add("draggable");
 
                 // set the text to selectedText
                 newNotecard.innerHTML = selectedText;
 
+                // add handler for drag and drop
+                newNotecard.addEventListener('mousedown', mouseDownDragHandler);
+
                 // add notecard to the notecards section
                 document.querySelector("#notecards").appendChild(newNotecard);
+
             }
         }
     }
 });
+
+// drag and drop code - used code here: https://htmldom.dev/drag-and-drop-element-in-a-list/
+
+// The current dragging item
+let draggingEle;
+
+// The current position of mouse relative to the dragging element
+let x = 0;
+let y = 0;
+let z = 10;
+
+const mouseDownDragHandler = function (e) {
+    draggingEle = e.target;
+
+    // Calculate the mouse position
+    const rect = draggingEle.getBoundingClientRect();
+    x = e.pageX - rect.left;
+    y = e.pageY - rect.top;
+
+    // set to absolute positioning
+    draggingEle.style.position = 'absolute';
+
+    // increment z and put item on top
+    z++;
+    draggingEle.style.zIndex = z;
+
+    // Attach the listeners to `document`
+    document.addEventListener('mousemove', mouseMoveDragHandler);
+    document.addEventListener('mouseup', mouseUpDragHandler);
+};
+
+const mouseMoveDragHandler = function (e) {
+    // Set position for dragging element
+    draggingEle.style.top = `${e.pageY - y}px`;
+    draggingEle.style.left = `${e.pageX - x}px`;
+};
+
+const mouseUpDragHandler = function() {
+    //clone element to remove all the event listeners
+    var old_element = draggingEle;
+    var new_element = old_element.cloneNode(true);
+    old_element.parentNode.replaceChild(new_element, old_element);
+
+    // add mousedown listener
+    new_element.addEventListener('mousedown', mouseDownDragHandler);
+};
